3a32e0491afbab99884ab42d49d32edd52bee0a1
altariwing <altariwing@gmail.com>
2017-08-11 21:08:02 +0800

    adding js to hw10
    isLEAPyear?

--- HW10/HW10js.js
+++ HW10/HW10js.js
@@ -5,3 +5,16 @@
  * @version $Id$
  */
 
+var dateObject = new Date;
+var year = dateObject.getFullYear();
+var month = dateObject.getMonth() + 1; //月份0~11
+var date = dateObject.getDate()
+var day = dateObject.getDay(); //星期0~6(日到六)
+
+function isLeapYear(year) {
+	return (year%400==0) || ((year%4==0) && (year%100!=0));
+}
+
+function iamaCalendar() {
+    document.getElementById("caption").innerHTML = Date();
+}
